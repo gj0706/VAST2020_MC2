c9d21b73434230d4da40ff6281da7823c40fec0e
Jian Guo <guojian0706@gmail.com>
2020-07-07 14:14:51 -0500

    Updated concept map with ground truth data

--- js/network.js
+++ js/network.js
@@ -50,126 +50,7 @@ const config = {
 
 
 
-// Load the data
-d3.json("data/data.json").then(function(data){
-    // d3.json("data/gt.json").then(function(gt) {
-    d3.json("data/pca.json").then(function(pcaData){
-
-
-
-        // nest data
-        let innerNode = _.values(nest(data, ['PersonId', 'Label']));
-        let outerNode = nest(data, ['Label', 'PersonId']);
-        let grouped = _.values(nest(data, ['PersonId', "ImageId"]));
-        let nested = _.map(grouped, (d, i) => {
-            return {
-                "user": `user-${i + 1}`, "images": _.map(_.keys(d), v => {
-                    return {
-                        "imageId": v, "objects": _.map(d[v], o => {
-                            return {"label": o.Label, "info": o}
-                        })
-                    }
-                })
-            }
-        })
-        console.log(nested);
-
-        // Extract ground truth data
-        // let gtData = nest(gt, ["External ID"]);
-        // console.log(gt);
-        // _.map(gt, d=>{return {"user": d["External ID"].split(".")[0].slice(6).split("_")[0], "imageId": d["External ID"].split(".")[0].slice(6), "objects": d.Label.objects}})
-        // let groupedGt = _.map(gt, d=>{return {"user": d["External ID"].split(".")[0].slice(6).split("_")[0], "imageId": d["External ID"].split(".")[0].slice(6), "objects": _.map(d.Label.objects, v=>{return {"name": v.title, "bbox": v.bbox}})}})
-        // let nestedGt = _.values(nest(groupedGt, ["user", "imageId"]));
-        // console.log(groupedGt);
-        // console.log(innerNode);
-        // document.write(JSON.stringify(nested, null, 4));
-        // let arr = _.values(_.mapKeys(nested, function(value, key) { value.id = key; return value; }));
-        // console.log(arr);
-        // let n = _.values(innerNode.map((d, i)=>{return{"node": (i+1).toString(), "links": _.values(d)}}));
-        // console.log(n);
-
-        let links = [];
-        innerNode.forEach((d, i) => {
-
-            d["node"] = `user-${(i + 1).toString()}`;
-            d["links"] = _.keys(d).filter(d => d !== "node").map((v) => {
-                return {"source": `user-${(i + 1).toString()}`, "target": v, "width": d[v].length}
-            });
-            d["info"] = Object.values(d)
-            links.push(_.keys(d).filter(d => d !== "node").map((v) => {
-                return {"source": `user-${(i + 1).toString()}`, "target": v, "width": d[v].length}
-            }));
-            d["relatedNodes"] = _.keys(d).filter(v => (v !== "node") && (v !== "links"));
-            d["relatedNodes"].push(d["node"]);
-            d["relatedLinks"] = d["relatedNodes"].map(v => d["node"] + "-" + v);
-        });
-
-
-        // Transform data for outer nodes
-        outerNode = _.map(outerNode, (obj, key) => {
-            obj.node = key;
-            return obj
-        });
-
-        outerNode.forEach((d) => {
-            // d["node"] = d["Label"],
-            d["links"] = _.keys(d).filter(d => (d !== "node") && (d !== "links")).map((v, i) => {
-                return {"source": d.node, "target": `user-${v}`, "width": d[v].length}
-            });
-            d["relatedNodes"] = _.keys(d).filter(d => (d !== "node") && (d !== "links")).map(v => `user-${v}`);
-            d["relatedLinks"] = d["relatedNodes"].filter(d => (d !== "node") && (d !== "links")).map(v => `${v}-${d["node"]}`);
-            d["info"] = Object.values(d)
-        });
-
-        // Data for concept map
-        data = {"inner": innerNode, "outer": outerNode}
-
-        // Initialize bar charts
-        let user1 = nested.filter(d => d.user === "user-1")[0].images;
-        drawBarchart(user1, "#barChart");
-        for (let i = 0; i < user1.length; i++) {
-            appendImages(user1[i], "#images");
-        }
-
-        // Create drop down menu for each user
-        let users = [];
-        for (let i = 1; i < 41; i++) {
-            users.push(`user-${i}`);
-        }
-
-        // Function in response to dropdown event listener
-        let dropDownChange = function () {
-            let selected = d3.select(this).property("value");
-            let newData = _.filter(nested, d => d.user === selected)[0].images;
-            d3.select("#barChart").selectAll("svg").remove();
-
-            drawBarchart(newData, "#barChart");
-            d3.select("#images").selectAll("svg").remove();
-
-            for (let i = 0; i < newData.length; i++) {
-                appendImages(newData[i], "#images");
-            }
-        }
-
-        // Create dropdown selections and add event listener
-        let selection = d3.select("#selection")
-            .insert("select", "svg")
-            .on("change", dropDownChange)
-        selection.selectAll("option")
-            .data(users)
-            .enter().append("option")
-            .attr("value", d => d)
-            .text(d => d);
-
-        // Draw network of users and items
-        drawConMap(data, "#conMap");
-    // })
-
-        drawPCA(pcaData, "#pca");
-
-    })
-})
-
+// Draw network using concept map
 
 function drawConMap(data, selector){
     let svg = d3.select(selector).append("svg")
@@ -261,7 +142,7 @@ function drawConMap(data, selector){
         .attr("id", d=>`${d.source}-${d.target}`)
         .attr("fill", "none")
         .attr("stroke", "#457b9d")
-        .attr("stroke-width", d=>d.width/6)
+        .attr("stroke-width", d=>d.width)
         // .attr("d", d=>diagonal(d))
         .attr("d", link);
 
@@ -371,9 +252,9 @@ function mouseout(d){
 }
 
 
-function showLargeImage(thumbnail) {
-    document.getElementById('wup').src = thumbnail.src
-    document.querySelector("#wup").style.visibility = "visible";
-    document.querySelector("button").style.visibility = "visible";
-    document.querySelector("#cover").style.visibility = "visible";
-}
+// function showLargeImage(thumbnail) {
+//     document.getElementById('wup').src = thumbnail.src
+//     document.querySelector("#wup").style.visibility = "visible";
+//     document.querySelector("button").style.visibility = "visible";
+//     document.querySelector("#cover").style.visibility = "visible";
+// }
